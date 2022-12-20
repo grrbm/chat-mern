7ad9d8f8561466b2d508bccb9b80b7ed80c8a27a
Guilherme Reis <grrbm2@gmail.com>
2022-12-20 16:29:15 -0300

    added some error handling

--- frontend/src/pages/JoinPage.js
+++ frontend/src/pages/JoinPage.js
@@ -1,55 +1,90 @@
-import React, { useState } from 'react'
-import { Redirect } from 'react-router-dom'
-import axios from 'axios'
-import Swal from 'sweetalert2'
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+import axios from "axios";
+import Swal from "sweetalert2";
 
-function JoinPage(){
-    const [toChatPage, setToChatPage] = useState(false);
-    function handleClick(e){
-        e.preventDefault();
-        async function signinUser(){
-            try {
-                const result = await axios({
-                    method:'post',
-                    url:'http://localhost:4000/auth/signin', 
-                    data: { 
-                        username: document.querySelectorAll('input')[0].value, 
-                        password: document.querySelectorAll('input')[1].value
-                    }
-                });
-                setToChatPage(true);
-            } catch(e) {
-                if (e.toString().includes(400))
-                {                
-                    Swal.fire("Wrong password.");
-                }
-            }
+function JoinPage() {
+  const [toChatPage, setToChatPage] = useState(false);
+  function handleClick(e) {
+    e.preventDefault();
+    async function signinUser() {
+      try {
+        const result = await axios({
+          method: "post",
+          url: "http://localhost:4000/auth/signin",
+          data: {
+            username: document.querySelectorAll("input")[0].value,
+            password: document.querySelectorAll("input")[1].value,
+          },
+        });
+        setToChatPage(true);
+      } catch (error) {
+        console.log("Failed logging in.");
+        //Handling error
+
+        if (error.response) {
+          if (error.response.status === 401) {
+            Swal.fire(error.response.data.message);
+          } else if (error.toString().includes(400)) {
+            Swal.fire("Wrong password.");
+          } else {
+            Swal.fire(error.response.data.message);
+          }
+        } else if (error.request) {
+          Swal.fire(
+            "The request was made but no response was received. API_URL= " +
+              API_URL +
+              ", NODE_ENV= " +
+              process.env.REACT_APP_ENVIRONMENT
+          );
+          console.log(error.request);
+        } else {
+          Swal.fire("Internal Server Error !");
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
         }
-        signinUser();
+      }
     }
-    if (toChatPage === true) {
-        console.log("username: "+document.querySelectorAll('input')[0].value);
-        return <Redirect to={{
-            pathname: "/chat",
-            state: { username: document.querySelectorAll('input')[0].value, 
-                     room: 'DefaultRoom' }
-          }} />
-        
-    }
+    signinUser();
+  }
+  if (toChatPage === true) {
+    console.log("username: " + document.querySelectorAll("input")[0].value);
     return (
-        <div id="join-page" className="centered-form">
-            <div className="centered-form__box">
-                <h1>Join</h1>
-                <form>
-                    <label>Display name</label>
-                    <input type="text" name="username" placeholder="Display name" required={true}></input>
-                    <label>Password</label>
-                    <input type="password" name="password" placeholder="Password" required={true}></input>
-                    <button onClick={handleClick}>Join</button>
-                </form>
-            </div>
-        </div>
-    )
+      <Redirect
+        to={{
+          pathname: "/chat",
+          state: {
+            username: document.querySelectorAll("input")[0].value,
+            room: "DefaultRoom",
+          },
+        }}
+      />
+    );
+  }
+  return (
+    <div id="join-page" className="centered-form">
+      <div className="centered-form__box">
+        <h1>Join</h1>
+        <form>
+          <label>Display name</label>
+          <input
+            type="text"
+            name="username"
+            placeholder="Display name"
+            required={true}
+          ></input>
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            required={true}
+          ></input>
+          <button onClick={handleClick}>Join</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default JoinPage
+export default JoinPage;
